81ee007076863a17fc75ca6f5847ee2bda952589
Sam Martin Vargas Giagnocavo <samfefuty@hotmail.com>
2021-11-25 15:24:55 +0100

    New version, not working :(

--- agents.py
+++ agents.py
@@ -10,11 +10,30 @@ class UserAgent(Agent):
         self._name = None
         self._ogoal = "cost"
         self._devices = []
+        self._schedule = []
+        self._home_manager = None
+    
+    def run_devices(self, day, time):
+        for i, s in enumerate(self._schedule):
+            day_start, day_end = s[0]
+            hour_start, hour_end = s[1]
 
+            # Is the user using the device ?
+            # If so, discharge it.
+            if day >= day_start and day <= day_end \
+                and time >= hour_start and time <= hour_end:
+                print(f"{self._name} uses {self._devices[i].to_string()}")
+                self._devices[i].discharge()
+            # If not, plug it to the grid.
+            elif not self._devices[i].is_charging():
+                print(f"{self._name} plugs {self._devices[i].to_string()}")
+                # Calculate t2c by checking schedule
+                self._devices[i].start_charge(time, time+1.0)
+    
     def to_string(self):
         output = f"{self._name}/{self._ogoal}\n"
-        for d in self._devices:
-            output += f"\t↣ {d.to_string()}\n"
+        for i, d in enumerate(self._devices):
+            output += f"\t↣ {d.to_string()} active at {self._schedule[i]}\n"
         return output
 
 class GridAgent(Agent):
@@ -27,6 +46,16 @@ class GridAgent(Agent):
     def to_string(self):
         pass
 
+class HomeAgent(Agent):
+
+    """Docstring for Home. """
+
+    def on_init(self):
+        pass
+
+    def check_for_generator(self):
+        pass
+
 class DeviceAgent(Agent):
     
     """Docstring for Device. """
@@ -37,14 +66,24 @@ class DeviceAgent(Agent):
         self._name = None
         self._power_limit = 1
         self._capacity = 100
-        self._load_profile = {'x': 1.0, 'y': 1.0}
+        self._load_profile = 0.0001
         self._curr_charge = 0
         self._scharge = -1
         self._echarge = -1
-
+        self._charging = False
+        self._is_generator = False
+    
     def start_charge(self, now, t2c):
+        self._charging = True
         self._scharge = now
-        self._echarge = self.scharge + t2c
+        self._echarge = self._scharge + t2c
+    
+    # By default we decrease one unit
+    def discharge(self, t = 1):
+        self._curr_charge = max(0, self._curr_charge - self._load_profile * t)
+    
+    def is_charging(self):
+        return self._charging
 
     def to_string(self):
         return f"{self._did}/{self._name}/" \
--- sim.py
+++ sim.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python3
 
 import random
+import time
+import numpy as np
 from data import Device, User
 from osbrain import run_agent
 from osbrain import run_nameserver
-from agents import GridAgent, DeviceAgent, UserAgent
+from agents import GridAgent, DeviceAgent, HomeAgent, UserAgent
 
 if __name__ == '__main__':
 
     grid = None
     users, devices = [], []
-    
+
+    """
+     _    ___   _   ___    ___   _ _____ _
+    | |  / _ \ /_\ |   \  |   \ /_\_   _/_\
+    | |_| (_) / _ \| |) | | |) / _ \| |/ _ \
+    |____\___/_/ \_\___/  |___/_/ \_\_/_/ \_\
+
+    """
+
     User.load_users("Users.json")
     Device.load_devices("Devices.json")
 
     ns = run_nameserver()
 
     grid = run_agent('Grid', base=GridAgent)
-    
+
+    symbols = "\|/-"
+    symbidx = 0
+    print(f"Loading data... {symbols[symbidx]}", end='\r')
+
+    # For now we get the first 5 users
+    User.LUT = User.LUT[:5]
+
     for i, u in enumerate(User.LUT):
 
         # Create the user
@@ -25,18 +42,57 @@ if __name__ == '__main__':
 
         # Create the user's list of devices
         devices = []
-        for x in range(u['ndevices']):
+        for x in range(u['devices']):
             didx = random.randint(0, len(Device.LUT)-1)
             devices.append(run_agent(base=DeviceAgent))
             devices[x].set_attr(
                 _did=didx,_name=Device.LUT[didx]['name'],
                 _load_profile=Device.LUT[didx]['load_profile'],
                 _power_limit=Device.LUT[didx]['power_limit'],
-                _capacity=Device.LUT[didx]['capacity']
+                _capacity=Device.LUT[didx]['capacity'],
+                _is_generator=Device.LUT[didx]['generator'],
+                _curr_charge=Device.LUT[didx]['curr_charge'],
             )
-            
+
+        schedule = []
+        for s in u['schedule']:
+            days, times = s.split("/")
+            day_start, day_end = days.split("-")
+            hour_start, hour_end = times.split("-")
+
+            day_start = int(day_start)
+            day_end = int(day_end)
+            hour_start = float(hour_start)
+            hour_end = float(hour_end)
+
+            schedule.append(((day_start, day_end),(hour_start, hour_end)))
+
         # Set the attributes for the user agent
-        users[i].set_attr(_name=u['name'],_ogoal=u['ogoal'],_devices=devices)
-        print(users[i].to_string())
+        users[i].set_attr(
+            _name=u['name'],
+            _ogoal=u['ogoal'],
+            _devices=devices,
+            _schedule=schedule,
+            _home_manager=run_agent(base=HomeAgent)
+        )
+
+        symbidx = symbidx + 1 if symbidx < len(symbols) - 1 else 0
+        print(f"Loading data... {symbols[symbidx]}", end='\r')
+
+    """
+     ___ _   _ _  _    ___ ___ __  __
+    | _ \ | | | \| |  / __|_ _|  \/  |
+    |   / |_| | .` |  \__ \| || |\/| |
+    |_|_\\___/|_|\_|  |___/___|_|  |_|
+
+    """
+    step = 0.1
+    for d in range(0, 7):
+        print(f"Day nº{d} started!")
+        for t in np.arange(0.0, 24.0, step):
+            print(f"Time ⇒ {t}", end='\r')
+            for u in users:
+                u.run_devices(d, t)
+                time.sleep(5)
 
     ns.shutdown()
